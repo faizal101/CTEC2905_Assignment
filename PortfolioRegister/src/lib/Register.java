818e5efd596d020f01fd5d8a3b6409918dfb76e1
『　　』 <faizal_10@live.co.uk>
2018-12-11 17:28:05 +0000

    attempted B.1 a)

--- PortfolioRegister/src/lib/Register.java
+++ PortfolioRegister/src/lib/Register.java
@@ -1,5 +1,45 @@
 package lib;
 
-public class Register {
+import java.util.ArrayList;
 
+public class Register {
+	
+	// Fields
+	
+	private ArrayList<Name> Name = new ArrayList<Name>();
+		
+	// Constructors
+	
+	public Register() {
+		Name = " ";
+	}
+	
+	// Methods
+	
+	public void setAddRegister(ArrayList<Name> name) {
+		this.Name = name;
+	}
+	
+	public void setRemoveRegister() {
+		// TODO
+	}
+	
+	public void setRegisterEmpty() {
+		Name.clear();
+	}
+	
+	public Name getRegisterName() {
+		return Name.get(0);
+	}
+	
+	public int getRegisterSize() {
+		return Name.size();
+	}
+	
+	public boolean getRegisterEmpty() {
+		return (Name.isEmpty()) ? true : false;
+	}
+	
+	// TODO toString() method override
+	
 }
