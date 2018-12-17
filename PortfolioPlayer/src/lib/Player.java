36de4c27c2e55e41df22f75ef1eaabfcfcbef07a
『　　』 <faizal_10@live.co.uk>
2018-12-17 17:03:21 +0000

    JavaDocs all the things\!

--- PortfolioPlayer/src/lib/Player.java
+++ PortfolioPlayer/src/lib/Player.java
@@ -11,25 +11,27 @@ public class Player implements Comparable<Player>{
 	
 	// Constructors
 	
+	/**
+	 * Default constructor that creates an empty name and dice.
+	 */
 	public Player() {
 		this.name = new Name();
 		this.dice = new Die();
 	}
 	
 	/** 
-	 * Default constructor that accepts a name. 
-	 * @param The name that will be given.  
+	 * Creates a player with the given name. 
+	 * @param name The name the player will have.  
 	 */
-	
 	public Player(Name name) {
 		this.name = name;
 		this.dice = new Die();
 	}
 	
 	/**
-	 * Default constructor that accepts a name and a dice.
-	 * @param name The name that will be given.
-	 * @param PairOfDice Consists of 2 die. (?)
+	 * Creates a player with the given name and dice.
+	 * @param name The name the player will have.
+	 * @param dice The dice that is given to the player.
 	 */
 	public Player(Name name, Rollable dice) {
 		this.name = name;
@@ -39,24 +41,25 @@ public class Player implements Comparable<Player>{
 	// Methods
 	
 	/**
-	 * Sets the variable of `Name` to the inputed parameter.
-	 * @param name The name the Player should have. (?)
+	 * Sets the name of the player from the parameter passed.
+	 * @param name The name the player will have.
 	 */
 	public void setName(Name name) {
 		this.name = name;
 	}
 	
 	/**
-	 * Sets the number of pair of dice.
-	 * @param PairOfDice number.
+	 * Sets the number of dice from the parameter passed.
+	 * @param dice The number of dice the player will have.
 	 */
 	public void setPairOfDice(PairOfDice dice) {
 		this.dice = dice;
 	}
 	
 	/**
-	 * Sets the full name of a player.
-	 * @param Name Full name of player.
+	 * Sets the full name of the player.
+	 * The string is split with a space (" ").
+	 * @param Name the full name the player should have.
 	 */
 	public void setFullPlayerName(String Name) {
 		String[] out = Name.split(" ");
@@ -66,33 +69,49 @@ public class Player implements Comparable<Player>{
 	
 	/**
 	 * Returns the name of the player.
-	 * @return Name
+	 * @return name The name of the player.
 	 */
 	public Name getName() {
 		return name;
 	}
 	
 	/**
-	 * Returns the pair of dice.
-	 * @return PairOfDice
+	 * Returns the dice of the player.
+	 * @return dice Returns the dice of the player.
 	 */
 	public Rollable getRollable() {
 		return dice;
 	}
 	
+	/**
+	 * Rolls the dice.
+	 */
 	public void rollDice() {
 		this.dice.roll();
 	}
 	
+	/**
+	 * Gets the dice score.
+	 * @return The dice score
+	 */
 	public int getDiceScore() {
 		return this.dice.getScore();
 	}
 	
+	/**
+	 * Returns the textual representation of the Player's name and dice.
+	 * @return The textual representation of the Player's name and dice.
+	 */
 	@Override
     public String toString() {
     	return "Player:[" + name + ", Dice=" + dice + "]";
     }
-
+	
+	/**
+	 * Compares the name of the player to another player, that is passed.
+	 * @param o The player that should be compared to.
+	 * @return A negative integer if player is less than, a positive integer if greater or zero if equal.
+	 */
 	@Override
 	public int compareTo(Player o) {
 		return this.name.compareTo(o.name);
--- PortfolioRegister/src/lib/Register.java
+++ PortfolioRegister/src/lib/Register.java
@@ -12,24 +12,44 @@ public class Register implements Iterable<Name>{
 		
 	// Constructors
 	
+	/**
+	 * Default constructor that will a create a Register ArrayList of Name.
+	 */
 	public Register() {
 		Name = new ArrayList<Name>();
 	}
 	
 	// Methods
 	
+	/**
+	 * Adds the name to the Register.
+	 * @param name The name of the person.
+	 */
 	public void addName(Name name) {
 		this.Name.add(name);
 	}
 	
+	/**
+	 * Removes the name from the Register at position `i`
+	 * @param i The position in the Register that should be removed.
+	 * @return Removes the name in position `i` from the ArrayList.
+	 */
 	public Name removeName(int i) {
 		return Name.remove(i);
 	}
 	
+	/**
+	 * Clears all the entries in the ArrayList.
+	 */
 	public void clearRegister() {
 		Name.clear();
 	}
 	
+	/**
+	 * Searches for the family name in the Register.
+	 * @param search The family name that it should search for.
+	 * @return True if the name is found, otherwise False if it doesn't exist.
+	 */
 	public boolean searchRegisterByFamilyName(String search) {
 		boolean found = false;
 		for (Name n : Name) {
@@ -40,6 +60,11 @@ public class Register implements Iterable<Name>{
 		return found;
 	}
 	
+	/**
+	 * Iterates through the Register and counts how many occurrences of the first name ending with `c`
+	 * @param c The character that the first name should end with.
+	 * @return The number of times the first name ended with `c`.
+	 */
 	public int countFirstNameOccurrences(char c) {
 		int i = 0;
 		for (Name n : Name) {
@@ -50,27 +75,49 @@ public class Register implements Iterable<Name>{
 		return i;
 	}
 	
+	/**
+	 * Gets the name from the Register at position `i`.
+	 * @param i The position on the Register.
+	 * @return The name at position `i`.
+	 */
 	public Name getName(int i) {
 		return Name.get(i);
 	}
 	
+	/**
+	 * Gets the length of the Register.
+	 * @return The length of the Register.
+	 */
 	public int registerSize() {
 		return Name.size();
 	}
 	
+	/**
+	 * Checks if the Register is empty.
+	 * @return True if the Register is empty, otherwise False if it's not empty.
+	 */
 	public boolean isRegisterEmpty() {
 		return (Name.isEmpty()) ? true : false;
 	}
-	
+	/**
+	 * Sorts the Register alphabetically.
+	 */
 	public void sortRegister() {
 		Collections.sort(Name);;
 	}
-	
+	/**
+	 * Returns the textual representation of the name from the Register.
+	 * @return The textual representation of the name from the Register.
+	 */
 	@Override
 	public String toString() {
 		return "Register:[" + Name + "]";
 	}
-
+	
+	/**
+	 * Access each element in the Register in turn.
+	 * @return An element in the Register.
+	 */
 	@Override
 	public Iterator<Name> iterator() {
 		return Name.iterator();
